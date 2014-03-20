ff11f6f7f1524388c4a1cadd13115a253bef87ab
christoffer <christoffer@10.18.133.40>
2014-03-20 09:50:14 +0100

    Initizialized

---
+++ src/casablanca/Casablanca.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casablanca;
+
+/**
+ *
+ * @author christoffer
+ */
+public class Casablanca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
