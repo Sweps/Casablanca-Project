68f380be17657c5cb1e3b27c0796c73df4ed66d5
Charles-HP <Charles-HP@10.18.137.204>
2014-04-10 13:12:35 +0200

    Startede på at lave TravelAngecy klassen

---
+++ src/domain/TravelAngecy.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package domain;
+
+/**
+ *
+ * @author Charles
+ */
+public class TravelAngecy 
+{
+    private int companyId, companyPhone, version;
+    private String companyName, companyEmail;
+    
+    public TravelAngecy(String companyName, int companyPhone)
+    {
+    this.companyName = companyName;
+    this.companyPhone = companyPhone;
+    }
+    
+}
